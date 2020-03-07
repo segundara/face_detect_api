9955478c5b568364db20016883d5634fb4fb4eed
segundara <segundara@gmail.com>
2020-03-07 18:24:05 +0200

    server-side update

---
+++ README.md
@@ -0,0 +1,11 @@
+### Face Detection App - Backend
+
+This is the Backend part of a Face detection app project.
+
+This app makes use of this service:
+
+### Clarifai API service.
+
+And uses:
+
+### postgreSQL for the database.
--- server.js
+++ server.js
@@ -2,95 +2,40 @@ const express = require('express');
 const bodyParser= require('body-parser');
 const bcrypt= require('bcrypt-nodejs');
 const cors= require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : 'test',
+    database : 'postgres'
+  }
+});
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors())
 
-const database = {
-	users:[
-		{
-			id:'123',
-			name:'John',
-			password: 'cookies',
-			email:'john@gmail.com',
-			entries:0,
-			joined:new Date()
-		},
-		{
-			id:'124',
-			name:'Sally',
-			password: 'bananas',
-			email:'sally@gmail.com',
-			entries:0,
-			joined:new Date()
-		}
-	],
-	login: [
-		{
-			id: '987',
-			has: '',
-			email: 'john@gmail.com'
-		}
-	]
-}
-
 app.get('/', (req, res)=>{
 	res.send(database.users);
 })
 
-app.post('/signin', (req, res)=>{
-	if (req.body.email===database.users[0].email && req.body.password===database.users[0].password){
-		res.json('signin success');
-		//console.log('signin success');
-	} else{
-		res.status(400).json('error logging in');
-	}
-})
+app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
 
-app.post('/register', (req, res)=>{
-	const {email,name,password} = req.body;
-	
-	database.users.push({
-		id:'125',
-		name:name,
-		email:email,
-		password:password,
-		entries:0,
-		joined:new Date()
-	})
-	res.json(database.users[database.users.length-1]);
-})
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)} )
 
-app.get('/profile/:id', (req, res)=>{
-	const {id} = req.params;
-	let found = false;
-	database.users.forEach(user =>{
-		if (user.id === id) {
-			found = true;
-			return res.json(user);
-		}
-	})
-	if (!found) {
-			res.status(400).json('user not found');
-		}
-})
+app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
 
-app.post('/image', (req, res) =>{
-	const {id} = req.body;
-	let found = false;
-	database.users.forEach(user =>{
-		if (user.id === id) {
-			found = true;
-			user.entries++
-			return res.json(user.entries);
-		}
-	})
-	if (!found) {
-			res.status(400).json('user not found');
-		}
-})
+app.put('/image', (req, res) => {image.handleImage(req, res, db)})
+app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
 
 //bcrypt.hash("bacon", null, null, function(err, hash){
 //		console.log(hash);
